0326f0f4e2a3c1240f37d52fbadfd1ce0cb83230
liwei <401613869@qq.com>
2022-04-24 10:58:26 +0800

    md

--- src/main/java/com/practice/config/MyMvcConfig.java
+++ src/main/java/com/practice/config/MyMvcConfig.java
@@ -6,6 +6,10 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
@@ -20,6 +24,16 @@ public class MyMvcConfig implements WebMvcConfigurer {
         //注册拦截器，及拦截的请求和剔除哪些请求
         //我们还需要过滤静态资源，否侧样式显示不出
         //注意
+
+        Object o = Proxy.newProxyInstance(MyMvcConfig.class.getClassLoader(), MyMvcConfig.class.getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                
+                method.invoke(proxy,args);
+
+                return  proxy;
+            }
+        });
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**").
                 excludePathPatterns("/tologin","/static/mute/**", "/login", "/","/toregister","/register","/Register.html");
