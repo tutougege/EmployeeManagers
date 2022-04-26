32679171357cad70b43d4cd5e11346cb9474cf0f
liwei <401613869@qq.com>
2022-04-26 14:28:47 +0800

    mybatis

--- src/test/java/com/practice/test/test01.java
+++ src/test/java/com/practice/test/test01.java
@@ -2,39 +2,24 @@ package com.practice.test;
 
 import com.practice.dao.EmployeeDao;
 import com.practice.pojo.Employee;
-import org.apache.ibatis.io.Resources;
+import com.practice.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-
 /**
  * @author liwei
  * @create 2022-04-25-20:42
  **/
 public class test01 {
     @Test
-    public void testAllEmp(){
-        InputStream is = null;
-        try {
-            is = Resources.getResourceAsStream("mybatis-config.xml");
-            //读取mybatis和数据库的会话
-            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
-            SqlSession sqlSession = factory.openSession();
+    public void testAllEmp() {
+            SqlSession sqlSession = MybatisUtils.getSqlSession();
             //动态代理并用mapper中的sql语句实现对应方法
             EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
             List<Employee> employees = employeeDao.allEmp();
-            for (Employee e:
-                 employees) {
+            for (Employee e :
+                    employees) {
                 System.out.println(e);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
-}
